7201ab3a7342a7762032ca4755b65724ed85883f
JuanFz117 <davidvivienda2@gmail.com>
2025-04-23 07:08:17 -0500

    all main juan

---
+++ menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <fieldset>
+    <h1>Menu</h1>
+
+    <input type="text" value="oficinas" name="oficinas" id="oficinas" placeholder="oficinas" required><br><br>
+
+    <input type="text" value="cargos" id="cargos" name="cargos" placeholder="cargos" required><br><br>
+
+    <input type="text" value="empleados" name="empleados" id="empleados" placeholder="empleados"><br><br>
+
+    <input type="text" value="clientes" id="clientes" name="clientes" placeholder="clientes" required><br><br>
+
+    <input type="text" value="inmuebles" name="inmuebles" id="inmuebles" placeholder="inmuebles" required><br><br>
+
+    <input type="text"value="tipo de inmuebles" name="tipo de inmuebles" id="tipo de inmuebles" placeholder="tipo de inmuebles" required><br><br>
+
+    <input type="text" value="propietarios" name="propietarios" id="propietarios" placeholder="propietarios" required><br><br>
+
+    <input type="text" value="contratos" name="contratos" id="contratos" placeholder="contratos" required><br><br>
+
+    <input type="text" value="visitas" name="visitas" id="visitas" placeholder="visitas" required><br><br>
+
+    <input type="text" value="inspecciones" name="inspecciones" id="inspecciones" placeholder="inspecciones" required><br><br>
+    
+</fieldset>
+</body>
+</html>
