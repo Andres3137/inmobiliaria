5e4b43a01547f05f205efdb3d1fd9afbab2dcf04
JuanFz117 <davidvivienda2@gmail.com>
2025-04-23 07:15:25 -0500

    menu

--- menu.php
+++ menu.php
@@ -10,25 +10,25 @@
     <fieldset>
     <h1>Menu</h1>
 
-    <input type="text" value="oficinas" name="oficinas" id="oficinas" placeholder="oficinas" required><br><br>
+    <butt type="button" value="oficinas" name="oficinas" id="oficinas" placeholder="oficinas" required><br><br>
+    
+    <input type="button" value="cargos" id="cargos" name="cargos" placeholder="cargos" required><br><br>
 
-    <input type="text" value="cargos" id="cargos" name="cargos" placeholder="cargos" required><br><br>
+    <input type="button" value="empleados" name="empleados" id="empleados" placeholder="empleados" required><br><br>
 
-    <input type="text" value="empleados" name="empleados" id="empleados" placeholder="empleados"><br><br>
+    <input type="button" value="clientes" id="clientes" name="clientes" placeholder="clientes" required><br><br>
 
-    <input type="text" value="clientes" id="clientes" name="clientes" placeholder="clientes" required><br><br>
+    <input type="button" value="inmuebles" name="inmuebles" id="inmuebles" placeholder="inmuebles" required><br><br>
 
-    <input type="text" value="inmuebles" name="inmuebles" id="inmuebles" placeholder="inmuebles" required><br><br>
+    <input type="button"value="tipo de inmuebles" name="tipo de inmuebles" id="tipo de inmuebles" placeholder="tipo de inmuebles" required><br><br>
 
-    <input type="text"value="tipo de inmuebles" name="tipo de inmuebles" id="tipo de inmuebles" placeholder="tipo de inmuebles" required><br><br>
+    <input type="button" value="propietarios" name="propietarios" id="propietarios" placeholder="propietarios" required><br><br>
 
-    <input type="text" value="propietarios" name="propietarios" id="propietarios" placeholder="propietarios" required><br><br>
+    <input type="button" value="contratos" name="contratos" id="contratos" placeholder="contratos" required><br><br>
 
-    <input type="text" value="contratos" name="contratos" id="contratos" placeholder="contratos" required><br><br>
+    <input type="button" value="visitas" name="visitas" id="visitas" placeholder="visitas" required><br><br>
 
-    <input type="text" value="visitas" name="visitas" id="visitas" placeholder="visitas" required><br><br>
-
-    <input type="text" value="inspecciones" name="inspecciones" id="inspecciones" placeholder="inspecciones" required><br><br>
+    <input type="button" value="inspecciones" name="inspecciones" id="inspecciones" placeholder="inspecciones" required><br><br>
     
 </fieldset>
 </body>
